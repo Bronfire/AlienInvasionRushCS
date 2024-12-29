0a13408708fb86eaf52d12ad2a0a697374df1b6d
Celso Sinlao <csinlao@myseneca.ca>
2024-12-28 21:27:43 -0500

    added instance of class Clock

--- PythonWork/AlienInvasionCS/alien_invasion.py
+++ PythonWork/AlienInvasionCS/alien_invasion.py
@@ -9,3 +9,16 @@ if __name__ == '__main__':
     # Make the game instance, run the game
     ai = AlienInvasion()
     ai.run_game()
+
+
+
+
+def __init__(self):
+    '''Initialize the game, and create game resources.'''
+    pygame.init()
+
+def run_game(self):
+    '''Start the main loop for the game.'''
+    while True:
+        pygame.display.flip()
+        self.clock.tick(60)
