d1fecf0309998823e5facc3f6b1b1c30d753661e
Celso Sinlao <csinlao@myseneca.ca>
2024-12-29 18:32:41 -0500

    set up background color

--- PythonWork/AlienInvasionCS/alien_invasion.py
+++ PythonWork/AlienInvasionCS/alien_invasion.py
@@ -4,21 +4,31 @@ import pygame
 class AlienInvasion:
     '''This is a line comment'''
 
+    def __init__(self):
+        '''Initialize the game, and create game resources.'''
+        pygame.init()
+        self.clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+        # Set the background colour.
+        self.bg_color = (230, 230, 230)
+
+    def run_game(self):
+        '''Start the main loop for the game.'''
+        while True:
+            # Watch for the keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
 
 if __name__ == '__main__':
     # Make the game instance, run the game
     ai = AlienInvasion()
     ai.run_game()
-
-
-
-
-def __init__(self):
-    '''Initialize the game, and create game resources.'''
-    pygame.init()
-
-def run_game(self):
-    '''Start the main loop for the game.'''
-    while True:
-        pygame.display.flip()
-        self.clock.tick(60)
