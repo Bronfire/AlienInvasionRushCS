fda42dfa0ab62edcb9cc75637a5dd8cb9ef395a1
Celso Sinlao <csinlao@myseneca.ca>
2024-12-29 14:30:07 -0500

    added more details to ship.py

--- PythonWork/AlienInvasionCS/ship.py
+++ PythonWork/AlienInvasionCS/ship.py
@@ -6,4 +6,7 @@ class Ship:
         '''Initialize the ship'''
         self.screen = ai_game.screen
 
-    
+
+def blitme(self):
+    '''Draw the ship'''
+    self.screen.blit(self.image, self.rect)
