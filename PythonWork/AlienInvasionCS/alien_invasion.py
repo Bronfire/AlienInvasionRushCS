a198ccc9cd9107a9b9d5e45b707eeb768b89e6a9
Celso Sinlao <csinlao@myseneca.ca>
2024-12-28 21:22:24 -0500

    First commit of alien_invasion.py

---
+++ PythonWork/AlienInvasionCS/alien_invasion.py
@@ -0,0 +1,11 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    '''This is a line comment'''
+
+
+if __name__ == '__main__':
+    # Make the game instance, run the game
+    ai = AlienInvasion()
+    ai.run_game()
