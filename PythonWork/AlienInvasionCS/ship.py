1ba4ddfc5cb13bad1dfeaa9584c52d01b2000231
Celso Sinlao <csinlao@myseneca.ca>
2025-01-01 19:53:21 -0500

    updated ship so it uses the ship's speed in settings

--- PythonWork/AlienInvasionCS/ship.py
+++ PythonWork/AlienInvasionCS/ship.py
@@ -1,10 +1,12 @@
 import pygame
+from settings import Settings
 
 class Ship:
     '''A class to manage the ship'''
     def __init__(self, ai_game):
         '''Initialize the ship'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
@@ -15,16 +17,24 @@ class Ship:
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         '''update the ship's position based on the movement flag.'''
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+            #self.rect.x += 3
         if self.moving_left:
-            self.rect.x -= 1
+            #self.rect.x -= 3
+            self.x -= self.settings.ship_speed
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         '''Draw the ship'''
