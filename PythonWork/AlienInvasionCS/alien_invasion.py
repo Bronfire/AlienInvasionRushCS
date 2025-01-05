279bb30f3425c171eedbce80cb7ce69d0b2ed0ac
Celso Sinlao <csinlao@myseneca.ca>
2025-01-05 11:01:09 -0500

    making aliens move

--- PythonWork/AlienInvasionCS/alien_invasion.py
+++ PythonWork/AlienInvasionCS/alien_invasion.py
@@ -65,8 +65,13 @@ class AlienInvasion:
             self.ship.update()
             self.bullets.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
+
+    def _update_aliens(self):
+        '''Update the positions of all aliens in the fleet.'''
+        self.aliens.update()
 
     def _update_bullets(self):
             self.bullets.update()
