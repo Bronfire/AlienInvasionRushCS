db7191df8ddc841fc9692e458256cc0035a2e982
Celso Sinlao <csinlao@myseneca.ca>
2025-01-09 07:54:56 -0500

    ship loses lives if it crashes with alien

--- PythonWork/AlienInvasionCS/alien_invasion.py
+++ PythonWork/AlienInvasionCS/alien_invasion.py
@@ -1,7 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -21,6 +24,9 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height 
         pygame.display.set_caption("Alien Invasion")
 
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
+
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -29,6 +35,22 @@ class AlienInvasion:
 
         # Set the background colour.
         self.bg_color = (230, 230, 230)
+    
+    def _ship_hit(self):
+        '''Respond to the ship being hit by an alien.'''
+        # Decrement ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any bullets and aliens left over.
+        self.bullets.empty()
+        self.aliens.empty()
+
+        # Create new aliens and new ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause.
+        sleep(0.5)
 
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
@@ -76,6 +98,7 @@ class AlienInvasion:
 
         # look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
             print("Ship hit!!!")
 
     def _update_bullets(self):
